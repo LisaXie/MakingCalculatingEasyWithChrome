33ca5aaf6ccf3ec7f4a02e4b5993f43b9c1b826a
LisaXie <lxie.ger@gmail.com>
2015-08-18 13:09:02 -0700

    Tries

--- popup.js
+++ popup.js
@@ -1,3 +1,12 @@
 ///<reference path="typings/jquery/jquery.d.ts" />
 $(function () {
+    $('#paste').click(function () { pasteSelection(); });
 });
+function pasteSelection() {
+    chrome.tabs.query({ active: true, windowId: chrome.windows.WINDOW_ID_CURRENT }, function (tab) {
+        chrome.tabs.sendMessage(tab[0].id, { method: "getSelection" }, function (response) {
+            var text = document.getElementById('calcBar');
+            text.innerHTML = response.data;
+        });
+    });
+}
--- popup.ts
+++ popup.ts
@@ -1,5 +1,16 @@
 ///<reference path="typings/jquery/jquery.d.ts" />
 
 $(function(){
-	
+  $('#paste').click(function(){pasteSelection();});
 });
+
+function pasteSelection() {
+  chrome.tabs.query({active:true, windowId: chrome.windows.WINDOW_ID_CURRENT}, 
+  function(tab) {
+    chrome.tabs.sendMessage(tab[0].id, {method: "getSelection"}, 
+    function(response){
+      var text = document.getElementById('calcBar'); 
+      text.innerHTML = response.data;
+    });
+  });
+}
